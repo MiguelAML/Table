69ce02e0cb3ce0c4b2d1dc0191ca337b3da39ffc
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-04-09 13:16:15 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -2,10 +2,10 @@ const jonathanDiv = document.getElementById('jonathan');
 const matiasDiv = document.getElementById('matias');
 const infoDiv = document.getElementById('info');
 
-jonathanDiv.addEventListene('click', function() {
+jonathanDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'This is Jonathan info';
 });
 
-matiasDiv.addEventListene('click', function() {
+matiasDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'This is Matias info';
 });
