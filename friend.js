29a994e4e596f6a0aabbcd8437a1b596a2c52d65
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-04-10 13:02:07 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -9,3 +9,23 @@ jonathanDiv.addEventListener('click', function() {
 matiasDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'This is Matias info';
 });
+
+document.getElementById("checkButton").addEventListener("click", function() {
+  let age = document.getElementById("_____").value;
+  let resultText = document.getElementById("_____");
+  let resultImage = document.getElementById("_____");
+
+  if (age < 15) {
+    resultText.innerText = "_____";
+    resultText.style.color = "red"; // change color
+    resultImage.src = "YOUR_BICYCLE_IMAGE_LINK_HERE";
+  } else if (age > 16 && age < 24) {
+    resultText.innerText = "_____";
+    resultText.style.color = "green";
+    resultImage.src = "YOUR_CAR_IMAGE_LINK_HERE";
+  } else if (age > 28) {
+    resultText.innerText = "_____";
+    resultText.style.color = "green";
+    resultImage.src = "YOUR_RENTAL_CAR_IMAGE_LINK_HERE";
+  }
+});
