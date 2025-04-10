25e0d60dc38ed794f2592d10f9185b06fd616e2c
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-04-10 13:15:21 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -18,12 +18,12 @@ document.getElementById("checkButton").addEventListener("click", function() {
   if (age < 15) {
     resultText.innerText = "You cannot drive";
     resultText.style.color = "red"; // change color
-    resultImage.src = "Yhttps://i.etsystatic.com/19543171/r/il/f13c94/4513785165/il_570xN.4513785165_h290.jpg";
-  } else if (age > 16 && age < 24) {
+    resultImage.src = "https://i.etsystatic.com/19543171/r/il/f13c94/4513785165/il_570xN.4513785165_h290.jpg";
+  } else if (age >= 16 && age <= 24) {
     resultText.innerText = "You can drive";
     resultText.style.color = "green";
     resultImage.src = "https://www.bmw-m.com/content/dam/bmw/marketBMW_M/www_bmw-m_com/all-models/model-navigation/bmw-m8-competition-coupe-flyout.png";
-  } else if (age > 28) {
+  } else if (age >= 25) {
     resultText.innerText = "You can drive and rent a car";
     resultText.style.color = "green";
     resultImage.src = "https://cloudfront-us-east-2.images.arcpublishing.com/reuters/WFO32LK3CRMRBHEIQLVYPZ2QSE.jpg";
