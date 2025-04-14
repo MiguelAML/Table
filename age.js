d6d61aecdf08065d988c2006c08a238f55db75ca
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-04-14 12:35:58 -0400

    Update age.js

--- age.js
+++ age.js
@@ -23,6 +23,12 @@ const container = document.getElementById("list-container");
 for (let i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = "This is item #" + i;
+  if (i <= 20) {
+    p.style.color = "blue";
+  } else {
+    p.style.color = "red";
+  }
+
   container.appendChild(p);
 }
 
